58acedffd58804ab5fc1cab29a36fbf8ac2da9df
Adam Jones <jones_adam@rocketmail.com>
2021-02-14 19:54:15 +0000

    Add final report

--- docs/finalreport/build.sh
+++ docs/finalreport/build.sh
@@ -1,7 +1,9 @@
 # Installation instructions (or at least what I did on Ubuntu 20.04):
 #   sudo apt install texlive-latex-extra
 #   sudo apt install texlive-bibtex-extra
-#   npm install -g gdoc2latex@3.0.2
+#   npm install -g gdoc2latex@3.0.3
+
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 f=index
@@ -17,7 +19,7 @@ gdoc2latex --input $f.html --output $f.tex --template-start start.tex --template
 # This is done as one chained command so if an early step fails the entire thing fails
 # You may question why we run latex three times? Nobody knows.
 # Serious answer: https://tex.stackexchange.com/a/53236
-pdflatex --shell-escape -interaction=nonstopmode $f \
-&& bibtex $f \
-&& pdflatex --shell-escape -interaction=nonstopmode $f \
-&& pdflatex --shell-escape -interaction=nonstopmode $f
+pdflatex --shell-escape -interaction=nonstopmode $f
+bibtex $f
+pdflatex --shell-escape -interaction=nonstopmode $f
+pdflatex --shell-escape -interaction=nonstopmode $f
