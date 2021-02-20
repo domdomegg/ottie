253d5656bc0b8658680f40f2c06905329d0b6c57
Adam Jones <jones_adam@rocketmail.com>
2021-02-20 21:26:14 +0000

    Update final report

--- docs/finalreport/build.sh
+++ docs/finalreport/build.sh
@@ -16,10 +16,10 @@ rm -f $f.aux $f.bbl $f.blg $f.dvi $f.log $f.pdf $f.toc $f.out $f.tex $f.bib
 
 gdoc2latex --input $f.html --output $f.tex --template-start start.tex --template-end end.tex
 
-# This is done as one chained command so if an early step fails the entire thing fails
+# We ignore earlier failures as they're probably lying
 # You may question why we run latex three times? Nobody knows.
 # Serious answer: https://tex.stackexchange.com/a/53236
-pdflatex --shell-escape -interaction=nonstopmode $f
-bibtex $f
-pdflatex --shell-escape -interaction=nonstopmode $f
+pdflatex --shell-escape -interaction=nonstopmode $f || true
+bibtex $f || true
+pdflatex --shell-escape -interaction=nonstopmode $f || true
 pdflatex --shell-escape -interaction=nonstopmode $f
