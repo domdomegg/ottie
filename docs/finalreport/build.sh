19d1013ed61de087aeeef5d4123ea102339dd115
Adam Jones <domdomegg+git@gmail.com>
2021-04-23 18:56:23 +0100

    Use latexmk for final report

--- docs/finalreport/build.sh
+++ docs/finalreport/build.sh
@@ -1,6 +1,5 @@
 # Installation instructions (or at least what I did on Ubuntu 20.04):
-#   sudo apt install texlive-latex-extra
-#   sudo apt install texlive-bibtex-extra
+#   sudo apt install texlive-latex-extra texlive-bibtex-extra latexmk
 
 set -e
 
@@ -13,10 +12,5 @@ cd $DIR
 # Clean old stuff
 rm -f $f.aux $f.bbl $f.blg $f.dvi $f.log $f.pdf $f.toc $f.out
 
-# We ignore earlier failures as they're probably lying
-# You may question why we run latex three times? Nobody knows.
-# Serious answer: https://tex.stackexchange.com/a/53236
-pdflatex --shell-escape -interaction=nonstopmode $f || true
-bibtex $f || true
-pdflatex --shell-escape -interaction=nonstopmode $f || true
-pdflatex --shell-escape -interaction=nonstopmode $f || true
+# Build latex doc
+latexmk -shell-escape -pdf
