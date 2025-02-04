17f4ff19f2b6136854bf8c26e4148d4a7c412e39
Adam Jones <domdomegg+git@gmail.com>
2025-02-04 01:27:09 +0000

    Add linting

--- algorithm-w-prime/src/index.ts
+++ algorithm-w-prime/src/index.ts
@@ -1,187 +1,216 @@
-import { TypeVar, TypeFuncApp, MonoType, PolyType, Context, Expr, Var, App, Abs, Let, CharLiteral, NumberLiteral, typeUtils, Response, TypeResult, TypeInferenceError, isTypeInferenceError, assertIsTypeInferenceError, Substitution, inst, unify, apply, combine, generalise, freeVars } from 'language'
+import {
+	TypeVar, TypeFuncApp, type MonoType, PolyType, type Context, type Expr, Var, App, Abs, Let, CharLiteral, NumberLiteral, typeUtils, type Response, type TypeResult, TypeInferenceError, isTypeInferenceError, assertIsTypeInferenceError, type Substitution, inst, unify, apply, combine, generalise, freeVars,
+} from 'language';
 
 function unifySubtitutions(s0: Substitution, s1: Substitution): Substitution {
-    // const d0 = diff(Object.keys(s0), Object.keys(s1));
-    // const d1 = diff(Object.keys(s1), Object.keys(s0));
-    const dn = Object.keys(s0).filter(k => k in s1);
+	// const d0 = diff(Object.keys(s0), Object.keys(s1));
+	// const d1 = diff(Object.keys(s1), Object.keys(s0));
+	const dn = Object.keys(s0).filter((k) => k in s1);
 
-    const t0 = Object.entries(s0).filter(([k, v]) => !(k in s1));
-    const t1 = Object.entries(s1).filter(([k, v]) => !(k in s0));
+	const t0 = Object.entries(s0).filter(([k]) => !(k in s1));
+	const t1 = Object.entries(s1).filter(([k]) => !(k in s0));
 
-    let s = t0.reduce<Substitution>((acc, cur) => { acc[cur[0]] = cur[1]; return acc; }, {});
-    for (let i = 0; i < t1.length; i++) {
-        const [alpha, tau] = [t1[i][0], t1[i][1]!];
-        const tauPrime = apply(s, tau);
-        if (freeVars(tauPrime).includes(alpha)) {
-            throw new TypeInferenceError('Occurs check failed. `' + alpha.toString() + '` occurs in `' + tauPrime.toString() + '` so unifying them would create an infinite type.');
-        }
-        s[alpha] = tauPrime;
-    }
+	const s = t0.reduce<Substitution>((acc, cur) => {
+		acc[cur[0]] = cur[1];
+		return acc;
+	}, {});
+	for (const [alpha, tau] of t1) {
+		const tauPrime = apply(s, tau!);
+		if (freeVars(tauPrime).includes(alpha)) {
+			throw new TypeInferenceError(`Occurs check failed. \`${alpha.toString()}\` occurs in \`${tauPrime.toString()}\` so unifying them would create an infinite type.`);
+		}
 
-    let v = s;
-    for (let i = 0; i < dn.length; i++) {
-        const beta = new TypeVar(dn[i]);
-        const tau0 = apply(v, apply(s0, beta));
-        const tau1 = apply(v, apply(s1, beta));
-        if (freeVars(tau0).includes(dn[i])) {
-            throw new TypeInferenceError('Occurs check failed. `' + dn[i].toString() + '` occurs in `' + tau0.toString() + '` so unifying them would create an infinite type.');
-        }
-        if (freeVars(tau1).includes(dn[i])) {
-            throw new TypeInferenceError('Occurs check failed. `' + dn[i].toString() + '` occurs in `' + tau1.toString() + '` so unifying them would create an infinite type.');
-        }
-        v = combine(v, unify(tau0, tau1));
-    }
+		s[alpha] = tauPrime;
+	}
 
-    return v;
+	let v = s;
+	for (const d of dn) {
+		const beta = new TypeVar(d);
+		const tau0 = apply(v, apply(s0, beta));
+		const tau1 = apply(v, apply(s1, beta));
+		if (freeVars(tau0).includes(d)) {
+			throw new TypeInferenceError(`Occurs check failed. \`${d.toString()}\` occurs in \`${tau0.toString()}\` so unifying them would create an infinite type.`);
+		}
+
+		if (freeVars(tau1).includes(d)) {
+			throw new TypeInferenceError(`Occurs check failed. \`${d.toString()}\` occurs in \`${tau1.toString()}\` so unifying them would create an infinite type.`);
+		}
+
+		v = combine(v, unify(tau0, tau1));
+	}
+
+	return v;
 }
 
 function infer(expr: Expr): MonoType;
 function infer(expr: Expr, forResponse: true, ctx?: Context): Response<TypeResult, Omit<TypeResult, 'type'>>;
-function infer(expr: Expr, forResponse: boolean = false, ctx: Context = typeUtils.standardCtx): MonoType | Response<TypeResult, Omit<TypeResult, 'type'>> {
-    let typeCounter = 0;
-    const freshTypeName = (): string => "t" + typeCounter++;
-    if (!forResponse) return _infer(expr, ctx, freshTypeName)[0];
+function infer(expr: Expr, forResponse = false, ctx: Context = typeUtils.standardCtx): MonoType | Response<TypeResult, Omit<TypeResult, 'type'>> {
+	let typeCounter = 0;
+	// eslint-disable-next-line no-plusplus
+	const freshTypeName = (): string => `t${typeCounter++}`;
+	if (!forResponse) {
+		return _infer(expr, ctx, freshTypeName)[0];
+	}
 
-    const steps: { message: string, ast: Expr }[] = [];
-    const logger = (message: string, notes: Map<Expr, string>) => {
-        steps.push({ message, ast: cloneAst(expr, notes) })
-    }
+	const steps: {message: string; ast: Expr}[] = [];
+	const logger = (message: string, notes: Map<Expr, string>) => {
+		steps.push({message, ast: cloneAst(expr, notes)});
+	};
 
-    try {
-        return {
-            accepted: true,
-            value: {
-                type: _infer(expr, ctx, freshTypeName, logger)[0],
-                steps
-            }
-        }
-    } catch (e) {
-        return {
-            accepted: false,
-            value: {
-                steps
-            },
-            issuePosition: (isTypeInferenceError(e) && e.expr) ? e.expr.pos : expr.pos,
-            message: (e as Error).message
-        }
-    }
+	try {
+		return {
+			accepted: true,
+			value: {
+				type: _infer(expr, ctx, freshTypeName, logger)[0],
+				steps,
+			},
+		};
+	} catch (e) {
+		return {
+			accepted: false,
+			value: {
+				steps,
+			},
+			issuePosition: (isTypeInferenceError(e) && e.expr) ? e.expr.pos : expr.pos,
+			message: (e as Error).message,
+		};
+	}
 }
 
 const cloneAst = (expr: Expr, notes: Map<Expr, string>): Expr => {
-    if (expr instanceof CharLiteral) return new CharLiteral(expr.value, expr.pos, notes.get(expr));
-    if (expr instanceof NumberLiteral) return new NumberLiteral(expr.value, expr.pos, notes.get(expr));
-    if (expr instanceof Var) return new Var(expr.name, expr.pos, notes.get(expr));
-    if (expr instanceof App) return new App(cloneAst(expr.func, notes), cloneAst(expr.arg, notes), expr.pos, notes.get(expr));
-    if (expr instanceof Abs) return new Abs(expr.param, cloneAst(expr.body, notes), expr.pos, notes.get(expr));
-    if (expr instanceof Let) return new Let(expr.param, cloneAst(expr.def, notes), cloneAst(expr.body, notes), expr.pos, notes.get(expr));
-    
-    // Should be unreachable...
-    throw new Error('Internal error, this should never happen');
-}
+	if (expr instanceof CharLiteral) {
+		return new CharLiteral(expr.value, expr.pos, notes.get(expr));
+	}
+
+	if (expr instanceof NumberLiteral) {
+		return new NumberLiteral(expr.value, expr.pos, notes.get(expr));
+	}
+
+	if (expr instanceof Var) {
+		return new Var(expr.name, expr.pos, notes.get(expr));
+	}
+
+	if (expr instanceof App) {
+		return new App(cloneAst(expr.func, notes), cloneAst(expr.arg, notes), expr.pos, notes.get(expr));
+	}
+
+	if (expr instanceof Abs) {
+		return new Abs(expr.param, cloneAst(expr.body, notes), expr.pos, notes.get(expr));
+	}
+
+	if (expr instanceof Let) {
+		return new Let(expr.param, cloneAst(expr.def, notes), cloneAst(expr.body, notes), expr.pos, notes.get(expr));
+	}
+
+	// Should be unreachable...
+	throw new Error('Internal error, this should never happen');
+};
 
 const highlight = (expr: Expr): Map<Expr, string> => {
-    const notes = new Map();
-    notes.set(expr, 'highlight')
-    return notes;
+	const notes = new Map<Expr, string>();
+	notes.set(expr, 'highlight');
+	return notes;
+};
+
+const str = (substitution: Substitution, except?: string): string => (`{ ${Object.keys(substitution).filter((k) => k !== except).map((k) => `${k} ↦ ${substitution[k]!.toString()}`).join(', ')} }`).replace('{  }', '{}');
+
+function _infer(expr: Expr, ctx: Context, freshTypeName: () => string, logger: (message: string, notes: Map<Expr, string>) => void = () => {/* noop */}): [MonoType, Substitution] {
+	if (expr instanceof CharLiteral) {
+		logger(`We know the primitive \`${expr.toString()}\` is a \`Char\``, highlight(expr));
+		return [inst(new PolyType([], new TypeFuncApp('Char')), freshTypeName), {}];
+	}
+
+	if (expr instanceof NumberLiteral) {
+		logger(`We know the primitive \`${expr.toString()}\` is an \`Int\``, highlight(expr));
+		return [inst(new PolyType([], new TypeFuncApp('Int')), freshTypeName), {}];
+	}
+
+	if (expr instanceof Var) {
+		const type = ctx[expr.name];
+		if (!type) {
+			logger(`We try to look up the variable \`${expr.toString()}\` but find it is not in scope. We stop here as this is an error.`, highlight(expr));
+			const err = new TypeInferenceError(`\`${expr.name}\` is not in scope`, expr);
+			throw err;
+		}
+
+		const instantiatedType = inst(type, freshTypeName);
+
+		logger(`We can look up the variable \`${expr.toString()}\` and find it has type \`${type.toString()}\`${type.quantifiedVars.length ? `\nWe instantiate this type with fresh type variables to get \`${instantiatedType.toString()}\`` : ''}`, highlight(expr));
+
+		return [instantiatedType, {}];
+	}
+
+	if (expr instanceof App) {
+		const [funcType, funcSubstitution] = _infer(expr.func, ctx, freshTypeName, logger);
+		const [argType, argSubstitution] = _infer(expr.arg, ctx, freshTypeName, logger);
+		const t = new TypeVar(freshTypeName());
+
+		// Give an easier to read and understand message if we can, otherwise default to the more general case
+		const firstPartOfLogMessage1 = `We first determine the function and argument type separately as \`${funcType.toString()}\` and \`${argType.toString()}\` respectively.\nFor these we have substitutions \`${str(funcSubstitution)}\` and \`${str(argSubstitution)}\` to unify.\n\n`;
+
+		// S'
+		let unifiedSubsitution;
+		try {
+			unifiedSubsitution = unifySubtitutions(funcSubstitution, argSubstitution);
+		} catch (err) {
+			assertIsTypeInferenceError(err);
+			logger(`${firstPartOfLogMessage1}However, these two subsitutions cannot be unified. We stop here as this is an error.\n\nMore details:\n${err.message}`, highlight(expr));
+			err.expr = expr;
+			throw err;
+		}
+
+		const funcType2 = apply(unifiedSubsitution, funcType);
+		const argType2 = apply(unifiedSubsitution, argType);
+
+		// Give an easier to read and understand message if we can, otherwise default to the more general case
+		const firstPartOfLogMessage2 = `${firstPartOfLogMessage1}These can be unified as \`${str(unifiedSubsitution)}\`\n\n${(funcType instanceof TypeFuncApp && funcType.constructorName === '->')
+			? `The function must accept the expected argument type.\nThe function type after applying the unified substitution is \`${funcType2.toString()}\`\n\nTherefore we unify:\nFunction accepts \`${(funcType2 as TypeFuncApp).args[0].toString()}\`\nArgument has type \`${argType2.toString()}\`\n\n`
+			: `The function must accept the expected argument type.\n\nTherefore we unify:\nFunction type \`${funcType2.toString()}\`\nArgument to fresh type \`${new TypeFuncApp('->', argType2, t).toString()}\`\n\n`}`;
+
+		// V
+		let unifyingSubstitution;
+		try {
+			unifyingSubstitution = unify(funcType2, new TypeFuncApp('->', argType2, t));
+		} catch (err) {
+			assertIsTypeInferenceError(err);
+			logger(`${firstPartOfLogMessage2}However, these two types are not unifiable. We stop here as this is an error.\n\nMore details:\n${err.message}`, highlight(expr));
+			err.expr = expr;
+			throw err;
+		}
+
+		const exprType = apply(unifyingSubstitution, t);
+		logger(`${firstPartOfLogMessage2}This gives the final substitution \`${str(unifyingSubstitution, t.name)}\`\nAnd the function's return type as \`${exprType.toString()}\``, highlight(expr));
+		return [exprType, combine(funcSubstitution, unifiedSubsitution, unifyingSubstitution)];
+	}
+
+	if (expr instanceof Abs) {
+		const t = new TypeVar(freshTypeName());
+
+		logger(`Our function definition binds \`${expr.param}\` in the body to the fresh type \`${t.toString()}\``, highlight(expr));
+
+		const [bodyType, bodySubstitution] = _infer(expr.body, {...ctx, [expr.param]: new PolyType([], t)}, freshTypeName, logger);
+		const type = apply(bodySubstitution, new TypeFuncApp('->', t, bodyType));
+
+		logger(`${bodySubstitution[t.name] ? `We apply the substitution \`{ ${t.name} ↦ ${bodySubstitution[t.name]!.toString()} }\` to get the parameter's type \`${type.args[0].toString()}\`.\n` : ''}The return type is given by the function body's type \`${type.args[1].toString()}\`\nTherefore the overall type is \`${type.toString()}\``, highlight(expr));
+
+		return [type, bodySubstitution];
+	}
+
+	if (expr instanceof Let) {
+		const [defType, defSubstitution] = _infer(expr.def, ctx, freshTypeName, logger);
+		const generalisedDefType = generalise(apply(defSubstitution, ctx), defType);
+
+		logger(`Our let statement binds \`${expr.param}\` in the body to the type \`${generalisedDefType.toString()}\``, highlight(expr));
+
+		const [bodyType, bodySubstitution] = _infer(expr.body, {...apply(defSubstitution, ctx), [expr.param]: generalisedDefType}, freshTypeName, logger);
+
+		logger(`Our let statement then takes its body's type \`${bodyType.toString()}\``, highlight(expr));
+
+		return [bodyType, combine(defSubstitution, bodySubstitution)];
+	}
+
+	// Should be unreachable...
+	throw new Error('Internal error, this should never happen');
 }
 
-const str = (substitution: Substitution, except?: string): string => ('{ ' + Object.keys(substitution).filter(k => k !== except).map(k => k + ' ↦ ' + substitution[k]!.toString()).join(', ') + ' }').replace('{  }', '{}');
-
-function _infer(expr: Expr, ctx: Context, freshTypeName: () => string, logger: (message: string, notes: Map<Expr, string>) => void = () => {}): [MonoType, Substitution] {
-    if (expr instanceof CharLiteral) {
-        logger('We know the primitive `' + expr.toString() + '` is a `Char`', highlight(expr));
-        return [inst(new PolyType([], new TypeFuncApp('Char')), freshTypeName), {}];
-    }
-
-    if (expr instanceof NumberLiteral) {
-        logger('We know the primitive `' + expr.toString() + '` is an `Int`', highlight(expr));
-        return [inst(new PolyType([], new TypeFuncApp('Int')), freshTypeName), {}];
-    }
-
-    if (expr instanceof Var) {
-        const type = ctx[expr.name]
-        if (!type) {
-            logger('We try to look up the variable `' + expr.toString() + '` but find it is not in scope. We stop here as this is an error.', highlight(expr));
-            const err = new TypeInferenceError('`' + expr.name + '` is not in scope', expr);
-            throw err;
-        }
-        const instantiatedType = inst(type, freshTypeName);
-
-        logger('We can look up the variable `' + expr.toString() + '` and find it has type `' + type.toString() + '`' + (type.quantifiedVars.length ? '\nWe instantiate this type with fresh type variables to get `' + instantiatedType.toString() + '`' : ''), highlight(expr));
-        
-        return [instantiatedType, {}];
-    }
-
-    if (expr instanceof App) {
-        const [funcType, funcSubstitution] = _infer(expr.func, ctx, freshTypeName, logger);
-        const [argType, argSubstitution] = _infer(expr.arg, ctx, freshTypeName, logger);
-        const t = new TypeVar(freshTypeName());
-
-        // Give an easier to read and understand message if we can, otherwise default to the more general case
-        const firstPartOfLogMessage1 = 'We first determine the function and argument type separately as `' + funcType.toString() + '` and `' + argType.toString() + '` respectively.\nFor these we have substitutions `' + str(funcSubstitution) + '` and `' + str(argSubstitution) + '` to unify.\n\n';
-
-        // S'
-        let unifiedSubsitution;
-        try {
-            unifiedSubsitution = unifySubtitutions(funcSubstitution, argSubstitution);
-        } catch (err) {
-            assertIsTypeInferenceError(err);
-            logger(firstPartOfLogMessage1 + 'However, these two subsitutions cannot be unified. We stop here as this is an error.\n\nMore details:\n' + err.message, highlight(expr));
-            err.expr = expr;
-            throw err;
-        }
-
-        const funcType2 = apply(unifiedSubsitution, funcType);
-        const argType2 = apply(unifiedSubsitution, argType);
-
-        // Give an easier to read and understand message if we can, otherwise default to the more general case
-        const firstPartOfLogMessage2 = firstPartOfLogMessage1 + 'These can be unified as `' + str(unifiedSubsitution) + '`\n\n' + ((funcType instanceof TypeFuncApp && funcType.constructorName == '->')
-            ? 'The function must accept the expected argument type.\nThe function type after applying the unified substitution is `' + funcType2.toString() + '`\n\nTherefore we unify:\nFunction accepts `' + (funcType2 as TypeFuncApp).args[0].toString() + '`\nArgument has type `' + argType2.toString() + '`\n\n'
-            : 'The function must accept the expected argument type.\n\nTherefore we unify:\nFunction type `' + funcType2.toString() + '`\nArgument to fresh type `' + new TypeFuncApp("->", argType2, t).toString() + '`\n\n');
-
-        // V
-        let unifyingSubstitution;
-        try {
-            unifyingSubstitution = unify(funcType2, new TypeFuncApp("->", argType2, t))
-        } catch (err) {
-            assertIsTypeInferenceError(err);
-            logger(firstPartOfLogMessage2 + 'However, these two types are not unifiable. We stop here as this is an error.\n\nMore details:\n' + err.message, highlight(expr));
-            err.expr = expr;
-            throw err;
-        }
-        const exprType = apply(unifyingSubstitution, t)
-        logger(firstPartOfLogMessage2 + 'This gives the final substitution `' + str(unifyingSubstitution, t.name) + '`\nAnd the function\'s return type as `' + exprType.toString() + '`', highlight(expr));
-        return [exprType, combine(funcSubstitution, unifiedSubsitution, unifyingSubstitution)]
-    }
-
-    if (expr instanceof Abs) {
-        const t = new TypeVar(freshTypeName());
-
-        logger('Our function definition binds `' + expr.param + '` in the body to the fresh type `' + t.toString() + '`', highlight(expr));
-
-        const [bodyType, bodySubstitution] = _infer(expr.body, { ...ctx, [expr.param]: new PolyType([], t) }, freshTypeName, logger);
-        const type = apply(bodySubstitution, new TypeFuncApp("->", t, bodyType));
-
-        logger((bodySubstitution[t.name] ? 'We apply the substitution `{ ' + t.name + ' ↦ ' + bodySubstitution[t.name]!.toString() + ' }` to get the parameter\'s type `' + type.args[0].toString() + '`.\n' : '') + 'The return type is given by the function body\'s type `' + type.args[1].toString() + '`\nTherefore the overall type is `' + type.toString() + '`', highlight(expr));
-
-        return [type, bodySubstitution]
-    }
-
-    if (expr instanceof Let) {
-        const [defType, defSubstitution] = _infer(expr.def, ctx, freshTypeName, logger);
-        const generalisedDefType = generalise(apply(defSubstitution, ctx), defType);
-
-        logger('Our let statement binds `' + expr.param + '` in the body to the type `' + generalisedDefType.toString() + '`', highlight(expr));
-
-        const [bodyType, bodySubstitution] = _infer(expr.body, { ...apply(defSubstitution, ctx), [expr.param]: generalisedDefType }, freshTypeName, logger);
-
-        logger('Our let statement then takes its body\'s type `' + bodyType.toString() + '`', highlight(expr));
-        
-        return [bodyType, combine(defSubstitution, bodySubstitution)]
-    }
-
-    // Should be unreachable...
-    throw new Error('Internal error, this should never happen');
-}
-
-export { unifySubtitutions, infer };
+export {unifySubtitutions, infer};
--- language/test/typeUtilities.test.ts
+++ language/test/typeUtilities.test.ts
@@ -1,36 +1,42 @@
-import { test, expect } from 'vitest';
-import { TypeVar, TypeFuncApp, Var, App, Abs, typeUtils } from '../src/index';
-const { number, boolean, f, list, tuple, maybe, either, a, b, c, d } = typeUtils;
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {test, expect} from 'vitest';
+import {
+	TypeVar, TypeFuncApp, Var, App, Abs, typeUtils,
+} from '../src/index';
+const {number, boolean, f, list, tuple, maybe, either, a, b, c, d} = typeUtils;
 
 test('function currying', () => {
-    expect(f(number, number, number)).toEqual(f(number, f(number, number)));
-    expect(f(number, f(number, number))).toEqual(f(number, number, number));
+	expect(f(number, number, number)).toEqual(f(number, f(number, number)));
+	expect(f(number, f(number, number))).toEqual(f(number, number, number));
 });
 
 test('list helper works', () => {
-    expect(list(a)).toEqual(new TypeFuncApp('[]', new TypeVar('a')));
-    expect(list(number)).toEqual(new TypeFuncApp('[]', new TypeFuncApp('Int')));
-})
+	expect(list(a)).toEqual(new TypeFuncApp('[]', new TypeVar('a')));
+	expect(list(number)).toEqual(new TypeFuncApp('[]', new TypeFuncApp('Int')));
+});
 
 test('tuple helper works', () => {
-    expect(() => tuple(a)).toThrow();
-    expect(tuple(a, b)).toEqual(new TypeFuncApp(',', a, b));
-    expect(tuple(a, b, c)).toEqual(new TypeFuncApp(',,', a, b, c));
-    expect(tuple(a, b, c, d)).toEqual(new TypeFuncApp(',,,', a, b, c, d));
-    expect(tuple(a, b, c, d, a)).toEqual(new TypeFuncApp(',,,,', a, b, c, d, a));
-    expect(tuple(a, b, c, d, a, b)).toEqual(new TypeFuncApp(',,,,,', a, b, c, d, a, b));
-    expect(tuple(a, b, c, d, a, b, c)).toEqual(new TypeFuncApp(',,,,,,', a, b, c, d, a, b, c));
-    expect(tuple(a, b, c, d, a, b, c, d)).toEqual(new TypeFuncApp(',,,,,,,', a, b, c, d, a, b, c, d));
-    expect(() => tuple(a, b, c, d, a, b, c, d, a, b)).toThrow();
-})
+	expect(() => tuple(a)).toThrow();
+	expect(tuple(a, b)).toEqual(new TypeFuncApp(',', a, b));
+	expect(tuple(a, b, c)).toEqual(new TypeFuncApp(',,', a, b, c));
+	expect(tuple(a, b, c, d)).toEqual(new TypeFuncApp(',,,', a, b, c, d));
+	expect(tuple(a, b, c, d, a)).toEqual(new TypeFuncApp(',,,,', a, b, c, d, a));
+	expect(tuple(a, b, c, d, a, b)).toEqual(new TypeFuncApp(',,,,,', a, b, c, d, a, b));
+	expect(tuple(a, b, c, d, a, b, c)).toEqual(new TypeFuncApp(',,,,,,', a, b, c, d, a, b, c));
+	expect(tuple(a, b, c, d, a, b, c, d)).toEqual(new TypeFuncApp(',,,,,,,', a, b, c, d, a, b, c, d));
+	expect(() => tuple(a, b, c, d, a, b, c, d, a, b)).toThrow();
+});
 
 test('maybe helper works', () => {
-    expect(maybe(a)).toEqual(new TypeFuncApp('Maybe', new TypeVar('a')));
-    expect(maybe(number)).toEqual(new TypeFuncApp('Maybe', new TypeFuncApp('Int')));
-})
+	expect(maybe(a)).toEqual(new TypeFuncApp('Maybe', new TypeVar('a')));
+	expect(maybe(number)).toEqual(new TypeFuncApp('Maybe', new TypeFuncApp('Int')));
+});
 
 test('either helper works', () => {
-    expect(either(a, b)).toEqual(new TypeFuncApp('Either', new TypeVar('a'), new TypeVar('b')));
-    expect(either(a, number)).toEqual(new TypeFuncApp('Either', new TypeVar('a'), new TypeFuncApp('Int')));
-    expect(either(boolean, number)).toEqual(new TypeFuncApp('Either', new TypeFuncApp('Bool'), new TypeFuncApp('Int')));
-})
+	expect(either(a, b)).toEqual(new TypeFuncApp('Either', new TypeVar('a'), new TypeVar('b')));
+	expect(either(a, number)).toEqual(new TypeFuncApp('Either', new TypeVar('a'), new TypeFuncApp('Int')));
+	expect(either(boolean, number)).toEqual(new TypeFuncApp('Either', new TypeFuncApp('Bool'), new TypeFuncApp('Int')));
+});
